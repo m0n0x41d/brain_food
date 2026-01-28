fd602e6c1f77360afac2e596940db0733231ea12
Ivan Zakutnii <zakutnii.ivan@gmail.com>
2026-01-28 11:51:41 +0400

    fix: edgecase for knuth sec

--- python_snippets/sortings/submit/task3.py
+++ python_snippets/sortings/submit/task3.py
@@ -16,11 +16,17 @@ def InsertionSortStep(
 
 # O(log n) by both time and space
 def KnuthSequence(array_size: int) -> list[int]:
+    if array_size <= 0:
+        return []
+
     sequence = []
     x = 1
     while x < array_size:
         sequence.append(x)
         x = 3 * x + 1
+
+    if not sequence:
+        sequence.append(1)
 
     sequence.reverse()
     return sequence
